a9a65ca6550f8c26e66ef4648294ea554aa1a75a
Ranaco <ranasatyamraj@gmail.com>
2023-06-21 15:08:08 +0530

    fix: 🐛 Hotfix

--- client/next.config.js
+++ client/next.config.js
@@ -1,16 +1,16 @@
 const webpack = require("webpack");
 
-const { parsed: myEnv } = require("dotenv").config({
-  path: "/home/astara/Code/web/trivid/client/.env",
-});
+// const { parsed: myEnv } = require("dotenv").config({
+//   path: "/home/astara/Code/web/trivid/client/.env",
+// });
 
 module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
   reactStrictMode: true,
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
-    return config;
-  },
+  // webpack(config) {
+  //   config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
+  //   return config;
+  // },
 };
